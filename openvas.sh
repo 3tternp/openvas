85e70c231801408ae2ae94b9fac64760f948d731
astra <26215963+3tternp@users.noreply.github.com>
2024-11-26 12:58:27 +0545

    Update openvas.sh

--- openvas.sh
+++ openvas.sh
@@ -2,7 +2,6 @@
 
 # Display the menu
 while true; do
-    
   echo -e "\033[36m"
   echo -e "Choose the operation you want to perform:"
   echo -e "\033[31m"
@@ -15,7 +14,7 @@ while true; do
   echo -e "4. Exit"
   echo -e "\033[39m"
 
-  read count
+  read -p "Enter your choice: " count
 
   if [ "$count" -eq 1 ]; then
     # Install GVM
@@ -24,20 +23,33 @@ while true; do
     sudo gvm-check-setup
     sudo runuser -u _gvm -- gvm-manage-certs -a -f
     sudo gvm-start
+    
+    # Ask if user wants remote access enabled
+    read -p "Do you want to make the server accessible from the remote network? (yes/no): " enable_remote
+    if [ "$enable_remote" == "yes" ]; then
+      # Replace the line in the greenbone-security-assistant.service file
+      sudo sed -i 's/ExecStart=.*/ExecStart=\/usr\/sbin\/gsad --listen=0.0.0.0 --port=9392/' /usr/lib/systemd/system/greenbone-security-assistant.service
+      echo -e "\033[32mRemote access enabled. Restarting the service...\033[39m"
+      
+      # Reload systemd and restart the service
+      sudo systemctl daemon-reload
+      sudo systemctl restart greenbone-security-assistant.service
+    else
+      echo -e "\033[33mRemote access not enabled.\033[39m"
+    fi
 
   elif [ "$count" -eq 2 ]; then
     # Update GVM feeds
     sudo greenbone-feed-sync
 
-    elif [ "$count" -eq 3 ]; then
+  elif [ "$count" -eq 3 ]; then
     # Fix Your GVM-XX.XX installation is not yet complete
-    sudo -u postgres psql
+    sudo -u postgres psql <<EOF
 ALTER DATABASE template1 REFRESH COLLATION VERSION;
 \q
-exit
-sudo -u postgres /usr/share/gvm/create-postgresql-database
+EOF
+    sudo -u postgres /usr/share/gvm/create-postgresql-database
 
-    
   elif [ "$count" -eq 4 ]; then
     # Exit the script
     exit 0
